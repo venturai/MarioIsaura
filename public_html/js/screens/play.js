82329df7c7c3e02b1e5a396f3de364f9bd3d86dd
isaura ventura <isaura.ventura@yahoo.com>
2014-12-02 15:02:11 -0800

    adding layers to my map

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -28,6 +28,6 @@ game.PlayScreen = me.ScreenObject.extend({
         
         resetPlayer: function(x, y){
             var player = me.pool.pull("mario", x, y, {});
-            me.game.world.addChild(player, 3);
+            me.game.world.addChild(player, 30);
         }
 });
